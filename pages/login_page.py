fc5cc65fa22f54f171cb31e52706d7cdc7b38008
admin <vovnik98@mail.ru>
2020-09-24 01:58:04 +0300

    вариант с явной инициализацией страниц в теле теста, чтобы избежать лишних сложностей с циклическими зависимостями 2

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,12 +10,13 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # проверку на корректный url адрес
-        assert LoginPageLocators.LOGIN_URL == self.browser.current_url, "url адрес не совпадает"
+        current_url = self.browser.current_url
+        assert LoginPageLocators.LOGIN_URL == current_url, f"url адреса не совпадает 1) {LoginPageLocators.LOGIN_URL} 2) {current_url})"
 
     def should_be_login_form(self):
         # проверка на наличие формы авторизации
-        assert self.browser.find_element(LoginPageLocators.LOGIN_FORM), "Нет формы авторизации"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), f"Нет формы авторизации, не найден локатор {LoginPageLocators.LOGIN_FORM}"
 
     def should_be_register_form(self):
         # проверка на наличие формы регистрации
-        assert self.browser.find_element(LoginPageLocators.LOGIN_FORM), "Нет формы регистрации"
+        assert self.bis_element_present(*LoginPageLocators.REGISTER_FORM), "Нет формы регистрации"
