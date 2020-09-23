7fa92bb347f09f66be705c6030d27a8fd0159663
admin <vovnik98@mail.ru>
2020-09-23 23:07:07 +0300

    Новые селекторы к ссылке на логин

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "ul.nav.navbar-nav.navbar-right > li > a")
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "ul.nav.navbar-nav.navbar-right > li > a"), "Login link is not presented"
